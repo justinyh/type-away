afeaf5fe27ed745f6832885f8231bed3e961aea2
justinyh <justinyh@usc.edu>
2020-08-02 12:54:32 -0500

    Updates

---
+++ precache-manifest.52e6138bdd8fb262040c2f5ad792fe07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a84a0d1e4556915f436282891d980d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66488905e395d7ca6005",
+    "url": "/static/js/2.77389273.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.77389273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a1c2267e57a21457f7",
+    "url": "/static/js/main.9415543d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
