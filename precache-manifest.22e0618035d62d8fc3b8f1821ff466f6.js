fa34db3aa32091a5b5327a82b65fff82d1aecd66
justinyh <justinyh@usc.edu>
2020-08-02 14:01:35 -0500

    Updates

---
+++ precache-manifest.22e0618035d62d8fc3b8f1821ff466f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a979c48dac8de11f83cbf6b5b6ff621",
+    "url": "/type-away/index.html"
+  },
+  {
+    "revision": "5e16dc728ae5e0cfe7d4",
+    "url": "/type-away/static/js/2.e2e03c89.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/type-away/static/js/2.e2e03c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a5fb3ab3589d1f8f1d6",
+    "url": "/type-away/static/js/main.b19447f4.chunk.js"
+  },
+  {
+    "revision": "b1eb16abd4e669d06680",
+    "url": "/type-away/static/js/runtime-main.1f6e03e5.js"
+  }
+]);
